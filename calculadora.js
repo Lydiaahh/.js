f0fb01b824315bd4b640f145e63ae4ea760cfe24
Lydia <116768885+Lydiaahh@users.noreply.github.com>
2025-10-21 23:44:22 -0300

    Create calculadora.js

---
+++ calculadora.js
@@ -0,0 +1,17 @@
+function soma(a, b){
+  return a + b
+}
+function subtrai(a, b){
+  return a - b
+}
+function multiplica(a, b){
+  return a * b
+}
+function divide(a,b){
+  return a / b
+}
+
+console.log(soma(10, 20))
+console.log(subtrai(10, 20))
+console.log(multiplica(10, 20))
+console.log(divide(10, 20))
